f13d725273c37a7f28b53b35216560f7f39355e2
Thành Đạt Lưu <luuthanhdat19022021@gmail.com>
2024-10-02 16:32:45 +0700

    Modify in _Layout and Index .cshtml

--- FU_Library_Web/obj/Debug/net8.0/FU_Library_Web.AssemblyInfo.cs
+++ FU_Library_Web/obj/Debug/net8.0/FU_Library_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FU_Library_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b4390bf0ea9eec82d3628d4994ae1b1b68fceb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbaca58e5f421aa54031ff31c61b98fa31999121")]
 [assembly: System.Reflection.AssemblyProductAttribute("FU_Library_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FU_Library_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
