141d570c08897d0252b03e8ef730fe4b86276b6b
Doan Minh Tri <doanminhtri008@gmail.com>
2024-11-11 22:24:18 +0700

    Revert "Merge branch 'main' into auth"
    This reverts commit 1af1c69d5cac7c99de892e3fe9fe1c9ad14ee532, reversing
    changes made to 4ee5dcd53eafd398c27d3041ec3233d58fabc7e4.

--- FU_Library_Web/Areas/News/Pages/Create.cshtml.cs
+++ FU_Library_Web/Areas/News/Pages/Create.cshtml.cs
@@ -1,4 +1,3 @@
-using DataAccess.Repository.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,29 +5,8 @@ namespace FU_Library_Web.Areas.News.Pages
 {
     public class CreateModel : PageModel
     {
-        private readonly INewsRepository _newRepository;
-        public string message { get; set; }
-        
-
-        [BindProperty]
-        public FU_Library_Web.Models.News news { get; set; }
-
         public void OnGet()
         {
-        }
-        
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            await _newRepository.AddNewsAsyns(news);
-
-            message = "News added successfully!";
-
-            return RedirectToPage("./Create");
         }
     }
 }
