f5121450a57cb56c8253ffe467fe99e519feaae7
datlt9 <datlt@var-meta.com>
2024-11-11 22:16:12 +0700

    Crud Catagories, Author

---
+++ FU_Library_Web/Pages/Authobook/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FU_Library_Web;
+using FU_Library_Web.Models;
+
+namespace FU_Library_Web.Pages.Authobook
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly FU_Library_Web.DatabaseContext _context;
+
+        public DeleteModel(FU_Library_Web.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public BookAuthor BookAuthor { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookauthor = await _context.BookAuthors.FirstOrDefaultAsync(m => m.BookAuthorId == id);
+
+            if (bookauthor == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                BookAuthor = bookauthor;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookauthor = await _context.BookAuthors.FindAsync(id);
+            if (bookauthor != null)
+            {
+                BookAuthor = bookauthor;
+                _context.BookAuthors.Remove(BookAuthor);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ FU_Library_Web/Pages/Authobook/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FU_Library_Web;
+using FU_Library_Web.Models;
+
+namespace FU_Library_Web.Pages.Authobook
+{
+    public class EditModel : PageModel
+    {
+        private readonly FU_Library_Web.DatabaseContext _context;
+
+        public EditModel(FU_Library_Web.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public BookAuthor BookAuthor { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookauthor =  await _context.BookAuthors.FirstOrDefaultAsync(m => m.BookAuthorId == id);
+            if (bookauthor == null)
+            {
+                return NotFound();
+            }
+            BookAuthor = bookauthor;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(BookAuthor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookAuthorExists(BookAuthor.BookAuthorId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool BookAuthorExists(Guid id)
+        {
+            return _context.BookAuthors.Any(e => e.BookAuthorId == id);
+        }
+    }
+}
