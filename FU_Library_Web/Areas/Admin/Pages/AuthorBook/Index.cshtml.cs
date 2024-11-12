8be13a984a384bce051bf0dbe6c22c046e6b26fb
datlt9 <datlt@var-meta.com>
2024-11-12 21:33:40 +0700

    New Update

--- FU_Library_Web/Areas/Admin/Pages/AuthorBook/Index.cshtml.cs
+++ FU_Library_Web/Areas/Admin/Pages/AuthorBook/Index.cshtml.cs
@@ -1,5 +1,7 @@
 
 using DataAccess.Entity;
+using DataAccess.Repository.Implement;
+using DataAccess.Repository.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -8,18 +10,25 @@ namespace FU_Library_Web.Areas.Admin.Pages.AuthorBook
 {
     public class IndexModel : PageModel
     {
-        private readonly FU_Library_Web.DatabaseContext _context;
+        //private readonly FU_Library_Web.DatabaseContext _context;
+        private readonly IBookAuthorRepository _bookAuthorRepository;
 
-        public IndexModel(FU_Library_Web.DatabaseContext context)
+        /*public IndexModel(FU_Library_Web.DatabaseContext context)
         {
             _context = context;
+        }*/
+
+        public IndexModel(IBookAuthorRepository bookAuthorRepository)
+        {
+            _bookAuthorRepository = bookAuthorRepository;
         }
 
         public IList<BookAuthors> BookAuthor { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            BookAuthor = await _context.BookAuthors.ToListAsync();
+            //BookAuthor = await _context.BookAuthors.ToListAsync();
+            BookAuthor = await _bookAuthorRepository.GetAll();
         }
         public async Task<IActionResult> OnPostCreateAsync(string? authorName, string? authorDes)
         {
---
+++ FU_Library_Web/Pages/Chat/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FU_Library_Web.Areas.Chat
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
